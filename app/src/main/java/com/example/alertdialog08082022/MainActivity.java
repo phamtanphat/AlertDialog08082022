9d61ef0e084d4cc5aae4ad662b6a288238223169
Phat <phatdroid94@gmail.com>
2022-10-03 21:30:45 +0700

    show dialog

--- app/src/main/java/com/example/alertdialog08082022/MainActivity.java
+++ app/src/main/java/com/example/alertdialog08082022/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.alertdialog08082022;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Constraints;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,6 +52,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.layout_picker_color_dialog);
 
+                Window window = dialog.getWindow();
+
+                if (window != null) {
+                    window.setGravity(Gravity.CENTER);
+                    window.setLayout(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                }
+
+                dialog.show();
+            }
+        });
     }
 }
