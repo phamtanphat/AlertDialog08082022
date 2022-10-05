0974c0e4a721dfc658b7ad642a31a586d04ecb82
Phat <phatdroid94@gmail.com>
2022-10-05 20:55:13 +0700

    set max value color

--- app/src/main/java/com/example/alertdialog08082022/MainActivity.java
+++ app/src/main/java/com/example/alertdialog08082022/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.alertdialog08082022;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -52,6 +56,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnDialog.setOnClickListener(new View.OnClickListener() {
+            int colorAlphaValue = 0;
+            int colorRedValue = 0;
+            int colorGreenValue = 0;
+            int colorBlueValue = 0;
+            TextView tvHex, tvRgb, tvButtonOk, tvButtonCancel;
+            ImageView imgBackground;
             @Override
             public void onClick(View view) {
                 Dialog dialog = new Dialog(MainActivity.this);
@@ -69,11 +79,20 @@ public class MainActivity extends AppCompatActivity {
                 SeekBar seekBarRed = dialog.findViewById(R.id.seek_bar_red);
                 SeekBar seekBarGreen = dialog.findViewById(R.id.seek_bar_green);
                 SeekBar seekBarBlue = dialog.findViewById(R.id.seek_bar_blue);
-                TextView tvHex = dialog.findViewById(R.id.text_view_hex_color);
-                TextView tvRgb = dialog.findViewById(R.id.text_view_rgb_color);
-                TextView tvButtonOk = dialog.findViewById(R.id.text_view_ok);
-                TextView tvButtonCancel = dialog.findViewById(R.id.text_view_cancel);
+                tvHex = dialog.findViewById(R.id.text_view_hex_color);
+                tvRgb = dialog.findViewById(R.id.text_view_rgb_color);
+                tvButtonOk = dialog.findViewById(R.id.text_view_ok);
+                tvButtonCancel = dialog.findViewById(R.id.text_view_cancel);
+                imgBackground = dialog.findViewById(R.id.image_view_background);
 
+                // Set max value seekbar
+                int maxValueOfColor = 255;
+                seekBarAlpha.setMax(maxValueOfColor);
+                seekBarRed.setMax(maxValueOfColor);
+                seekBarGreen.setMax(maxValueOfColor);
+                seekBarBlue.setMax(maxValueOfColor);
+
+                // Event change seekbar
                 seekBarAlpha.setOnSeekBarChangeListener(listenerSeekBarChange);
                 seekBarRed.setOnSeekBarChangeListener(listenerSeekBarChange);
                 seekBarGreen.setOnSeekBarChangeListener(listenerSeekBarChange);
@@ -83,18 +102,38 @@ public class MainActivity extends AppCompatActivity {
             }
 
             private SeekBar.OnSeekBarChangeListener listenerSeekBarChange = new SeekBar.OnSeekBarChangeListener() {
+                @SuppressLint("DefaultLocale")
                 @Override
-                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                public void onProgressChanged(SeekBar seekBar, int colorValue, boolean b) {
                     switch (seekBar.getId()) {
                         case R.id.seek_bar_alpha:
+                            colorAlphaValue = colorValue;
                             break;
                         case R.id.seek_bar_red:
+                            colorRedValue = colorValue;
                             break;
                         case R.id.seek_bar_green:
+                            colorGreenValue = colorValue;
                             break;
                         case R.id.seek_bar_blue:
+                            colorBlueValue = colorValue;
                             break;
                     }
+
+                    tvRgb.setText(String.format(
+                                    "RGB %d %d %d %d",
+                                    colorAlphaValue,
+                                    colorRedValue,
+                                    colorGreenValue,
+                                    colorBlueValue)
+                    );
+
+                    imgBackground.setBackgroundColor(Color.argb(
+                                                            colorAlphaValue,
+                                                            colorRedValue,
+                                                            colorGreenValue,
+                                                            colorBlueValue)
+                    );
                 }
 
                 @Override
